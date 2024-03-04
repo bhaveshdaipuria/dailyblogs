498aff7cfec9043a501e28d1c949b7cdb6be68da
Bhavesh Daipuria <bdaipuria@gmail.com>
2024-03-05 02:19:50 +0530

    setup env and config file for appwrite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import config from "./config/config.js";
+
+function App() {
+  console.log(config);
+  return (
+    <>
+      <h1>Blog with appwrite</h1>
+    </>
+  );
+}
+
+export default App;
