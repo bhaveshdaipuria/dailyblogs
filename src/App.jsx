e9a0eb102bdaa1f6387f43945c05a12d03b782ba
Bhavesh Daipuria <bdaipuria@gmail.com>
2024-03-08 06:20:29 +0530

    setup redux and basic files and directories for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import config from "./config/config.js";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(config);
-  return (
-    <>
-      <h1>Blog with appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
