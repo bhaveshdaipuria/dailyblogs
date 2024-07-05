1669779b60821581c82695eecee92853664a20aa
Bhavesh Daipuria <bdaipuria@gmail.com>
2024-07-05 11:00:53 +0530

    bug: Outlet tag in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import authService from "./appwrite/auth";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -28,7 +29,7 @@ function App() {
       <div className="w-full block">
         <Header />
         <main>
-          <liseOutlet />
+          <Outlet />
         </main>
         <Footer />
       </div>
